709d3cf204c2e938af761aef45c0be6cd9678ef6
unknown <chltjdrhd777@naver.com>
2020-11-25 18:23:54 +0900

    done

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import reduxThunk from "redux-thunk";
+import { mainReducer } from "./mainReducer";
+
+const middleware = [logger, reduxThunk];
+
+export const createStore = () => {
+  const store = configureStore({
+    reducer: mainReducer,
+    middleware,
+  });
+
+  return store;
+};
